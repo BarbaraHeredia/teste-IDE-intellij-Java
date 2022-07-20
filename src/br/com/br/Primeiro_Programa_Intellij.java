674e11f8b38e5106ef5dd256fb9cdeee05942736
BarbaraHeredia <barbaraheredia.es@gmail.com>
2022-07-20 09:51:06 -0300

    first commit

---
+++ src/br/com/br/Primeiro_Programa_Intellij.java
@@ -0,0 +1,17 @@
+package br.com.br;
+
+import br.com.br.model.Gato;
+
+public class Primeiro_Programa_Intellij {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 10;
+
+        System.out.println("Hello Word!" + (a+b));*/
+    }
+}
